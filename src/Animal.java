b6b7f1cd0de8b091a0520febd0a61c2765d70634
gdeli <gatosogamer1231@gmail.com>
2026-04-20 17:12:44 -0300

    Q7

---
+++ src/Animal.java
@@ -0,0 +1,27 @@
+public class Animal {
+    private String nome;
+    private int idade;
+    private float peso;
+
+    public void EmitirSom(){
+        System.out.println("Som Animal!!");
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public int getIdade() {
+        return idade;
+    }
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+    public float getPeso() {
+        return peso;
+    }
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,20 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Pessoa p1 = new Pessoa();
+        Animal animal = new Animal();
+        Gato gato = new Gato();
+        Cachorro cachorro = new Cachorro();
 
-        System.out.println("Digite seu nome: ");
-        p1.setNome(sc.next());
-        System.out.println("Digite sua idade: ");
-        p1.setIdade(sc.nextInt());
-        System.out.println("Sua idade:" + p1.getIdade());
+        gato.setNome("Geraldo");
+        gato.setIdade(13);
+        gato.setPeso(8.5f);
 
+        cachorro.setNome("Oswaldo");
+        cachorro.setIdade(18);
+        cachorro.setPeso(5f);
+        cachorro.setRaca("Urubu");
+
+        cachorro.EmitirSom();
+        gato.EmitirSom();
     }
 }
