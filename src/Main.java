951820d11d2768a627132ea337eb94248a17354b
gdeli <gatosogamer1231@gmail.com>
2026-04-19 22:22:05 -0300

    Q2

--- src/Main.java
+++ src/Main.java
@@ -3,19 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Livro l1 = new Livro();
 
-        System.out.println("The Black Bird");
-        System.out.println("--------------");
-        System.out.println("Pagina " + l1.ExibirStatus() + " de 300 paginas");
-        l1.avancarPaginas();
-        l1.avancarPaginas();
-        l1.avancarPaginas();
-        l1.avancarPaginas();
-        System.out.println("Pagina " + l1.ExibirStatus() + " de 300 paginas");
-        l1.retrocederPaginas();
-        l1.retrocederPaginas();
-        System.out.print("Pagina " + l1.ExibirStatus() + " de 300 paginas");
 
     }
 }
