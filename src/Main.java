458ec80b3a8509696292137642a2b3d6e3f44ec6
gdeli <gatosogamer1231@gmail.com>
2026-04-20 16:33:04 -0300

    Q5

--- src/Main.java
+++ src/Main.java
@@ -3,32 +3,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Produto produto = new Produto();
-        int opcao;
+        Pessoa p1 = new Pessoa();
 
-        System.out.println("Digite o nome do produto:");
-        produto.setNome(sc.nextLine());
-        System.out.println("Digite o quantidade do produto:");
-        produto.setQuantidadeEmEstoque(sc.nextInt());
-        System.out.println("Digite o valor do produto:");
-        produto.setPreco(sc.nextDouble());
+        System.out.println("Digite seu nome: ");
+        p1.setNome(sc.next());
+        System.out.println("Digite sua idade: ");
+        p1.setIdade(sc.nextInt());
+        System.out.println("Sua idade:" + p1.getIdade());
 
-            do{
-                System.out.println("1.Adicionar estoque");
-                System.out.println("2.Remover estoque");
-                System.out.println("3.ver estoque");
-                System.out.println("4.Sair");
-                opcao = sc.nextInt();
-                switch(opcao){
-                    case 1: System.out.println("Digite a quantidade que voce quer adicionar:");
-                            produto.AdicionarEstoque(sc.nextInt()); break;
-                    case 2: System.out.println("Digite a quantidade que voce remover:");
-                            produto.removerEstoque(sc.nextInt()); break;
-                    case 3: System.out.println("Quantidade em estoque:"+ produto.getQuantidadeEmEstoque()); break;
-
-                    case 4: System.out.println("Tchau"); break;
-
-                }
-            }while(opcao!=4);
     }
 }
---
+++ src/Pessoa.java
@@ -0,0 +1,23 @@
+public class Pessoa {
+    private String nome;
+    private int idade;
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public int getIdade() {
+        return idade;
+    }
+    public void setIdade(int idade) {
+        if(idade>=0 && idade<=120){
+            this.idade = idade;
+        }else{
+            System.out.println("ERRO - IDADE INVALIDA");
+        }
+
+    }
+
+}
