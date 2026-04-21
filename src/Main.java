8c1ca620f4ef7d99c65e8a1fdb5e2b5cc9b980b9
gdeli <gatosogamer1231@gmail.com>
2026-04-21 13:00:47 -0300

    Q13

--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Retangulo retangulo = new Retangulo();
-        Circulo circulo = new Circulo();
-
-        System.out.println("Digite o valor da largura e da altura do retangulo: ");
-        retangulo.setLargura(sc.nextDouble());
-        retangulo.setAltura(sc.nextDouble());
-        System.out.println("Digite o valor da raio do Circulo: ");
-        circulo.setRaio(sc.nextDouble());
-
-        System.out.println("area do circulo:" + circulo.calcularArea());
-        System.out.println("area do Retangulo:" + retangulo.calcularArea());
+        Calculadora calculadora = new Calculadora();
+        System.out.println("Dois numeros inteiros: " + calculadora.multiplicar(4, 1));
+        System.out.println("Tres numeros inteiros: " + calculadora.multiplicar(4, 2, 3));
+        System.out.println("Dois numeros decimais:" + calculadora.multiplicar(2.5, 2.5));
 
     }
 
